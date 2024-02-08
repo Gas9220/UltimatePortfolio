fa7887a2cb5bb4d3a3d9a2b8b8672d5bfbdf2a99
Gaspare <Gas9220@users.noreply.github.com>
2024-02-08 17:30:27 +0100

    feat: create IssueView

---
+++ UltimatePortfolio/Views/IssueView.swift
@@ -0,0 +1,49 @@
+//
+//  IssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Gaspare Monte on 08/02/24.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    @ObservedObject var issue: Issue
+
+    var body: some View {
+        Form {
+            Section {
+                VStack(alignment: .leading) {
+                    TextField("Title", text: $issue.issueTitle, prompt: Text("Enter the issue title here"))
+                        .font(.title)
+
+                    Text("**Modified** \(issue.issueModification.formatted(date: .long, time: .shortened))")
+                        .foregroundStyle(.secondary)
+
+                    Text("**Status**: \(issue.issueStatus)")
+                        .foregroundStyle(.secondary)
+                }
+
+                Picker("Priority", selection: $issue.priority) {
+                    Text("Low").tag(Int16(0))
+                    Text("Medium").tag(Int16(1))
+                    Text("High").tag(Int16(2))
+                }
+            }
+
+            Section {
+                VStack(alignment: .leading) {
+                    Text("Basic Information")
+                        .font(.title2)
+                        .foregroundStyle(.secondary)
+
+                    TextField("Description", text: $issue.issueContent, prompt: Text("Enter the issue description here"), axis:.vertical)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    IssueView(issue: .example)
+}
