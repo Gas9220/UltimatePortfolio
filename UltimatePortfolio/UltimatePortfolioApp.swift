6ede1e809185d6388fb56032881797f0e456a88e
Gaspare <Gas9220@users.noreply.github.com>
2024-02-08 14:11:40 +0100

    Starter

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Gaspare Monte on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
