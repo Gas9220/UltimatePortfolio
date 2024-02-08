defdb7f96e898910e91e7e9eccf5a0f3da44f8d8
Gaspare <Gas9220@users.noreply.github.com>
2024-02-08 17:30:13 +0100

    feat: add selectedIssue property to DataController and show conditionally DetailView content

---
+++ UltimatePortfolio/Views/NoIssueView.swift
@@ -0,0 +1,26 @@
+//
+//  NoIssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Gaspare Monte on 08/02/24.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    @EnvironmentObject var dataController: DataController
+
+    var body: some View {
+        Text("No Issue Selected")
+            .font(.title)
+            .foregroundStyle(.secondary)
+
+        Button("New Issue") {
+            
+        }
+    }
+}
+
+#Preview {
+    NoIssueView()
+}
