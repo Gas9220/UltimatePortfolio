e83b946ee75706f7229f919c66951120da4684e4
Gaspare <Gas9220@users.noreply.github.com>
2024-02-09 12:07:39 +0100

    feat: update IssueView to add and remove tags

--- UltimatePortfolio/Views/IssueView.swift
+++ UltimatePortfolio/Views/IssueView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct IssueView: View {
+    @EnvironmentObject var dataController: DataController
     @ObservedObject var issue: Issue
 
     var body: some View {
@@ -29,6 +30,35 @@ struct IssueView: View {
                     Text("Medium").tag(Int16(1))
                     Text("High").tag(Int16(2))
                 }
+
+                Menu {
+                    ForEach(issue.issueTags) { tag in
+                        Button {
+                            issue.removeFromTags(tag)
+                        } label: {
+                            Label(tag.tagName, systemImage: "checkmark")
+                        }
+                    }
+
+                    let otherTags = dataController.missingTags(from: issue)
+
+                    if otherTags.isEmpty == false {
+                        Divider()
+
+                        Section("Add Tags") {
+                            ForEach(otherTags) { tag in
+                                Button(tag.tagName) {
+                                    issue.addToTags(tag)
+                                }
+                            }
+                        }
+                    }
+                } label: {
+                    Text(issue.issueTagsList)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .animation(nil, value: issue.issueTagsList)
+                }
             }
 
             Section {
@@ -41,9 +71,11 @@ struct IssueView: View {
                 }
             }
         }
+        .disabled(issue.isDeleted)
     }
 }
 
 #Preview {
     IssueView(issue: .example)
+        .environmentObject(DataController.preview)
 }
