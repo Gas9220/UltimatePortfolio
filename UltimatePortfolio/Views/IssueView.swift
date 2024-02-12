13800d6269da68de135f9406e677320b11ebf41f
Gaspare <Gas9220@users.noreply.github.com>
2024-02-12 11:35:41 +0100

    feat: add Menu to IssueView to copy issue id and change status

--- UltimatePortfolio/Views/IssueView.swift
+++ UltimatePortfolio/Views/IssueView.swift
@@ -75,10 +75,30 @@ struct IssueView: View {
         .onReceive(issue.objectWillChange) { _ in
             dataController.queueSave()
         }
+        .toolbar {
+            Menu {
+                Button {
+                    UIPasteboard.general.string = issue.title
+                } label: {
+                    Label("Copy Issue Title", systemImage: "doc.on.doc")
+                }
+
+                Button {
+                    issue.completed.toggle()
+                    dataController.save()
+                } label: {
+                    Label(issue.completed ? "Re-open Issue" : "Close Issue", systemImage: "bubble.left.and.exclamationmark.bubble.right")
+                }
+            } label: {
+                Label("Actions", systemImage: "ellipsis.circle")
+            }
+        }
     }
 }
 
 #Preview {
-    IssueView(issue: .example)
-        .environmentObject(DataController.preview)
+    NavigationStack {
+        IssueView(issue: .example)
+            .environmentObject(DataController.preview)
+    }
 }
