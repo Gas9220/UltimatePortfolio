c10ca7e9d54f53597d3f55aad7a2546e3ad44257
Gaspare <Gas9220@users.noreply.github.com>
2024-02-12 10:51:15 +0100

    feat: award view first design

---
+++ UltimatePortfolio/Views/AwardView.swift
@@ -0,0 +1,40 @@
+//
+//  AwardView.swift
+//  UltimatePortfolio
+//
+//  Created by Gaspare Monte on 12/02/24.
+//
+
+import SwiftUI
+
+struct AwardView: View {
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(Award.allAwards) { award in
+                        Button {
+
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundStyle(.secondary.opacity(0.5))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Awards")
+        }
+    }
+}
+
+#Preview {
+    AwardView()
+}
